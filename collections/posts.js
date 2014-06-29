2eed9eb5d2e906037a97ad679db48a90663a9eac
bryan kennedy <bkennedy@smm.org>
2014-06-29 12:05:05 -0500

    Use a method to submit a post
    This allows us to add some checking on the submission

--- collections/posts.js
+++ collections/posts.js
@@ -1,7 +1,32 @@
 Posts = new Meteor.Collection('posts');
-Posts.allow({
-    insert: function(userId, doc) {
-        // Only allow posting if you're logged in
-        return !! userId;
+
+Meteor.methods({
+    post: function(postAttributes) {
+        var user = Meteor.user(),
+            postWithSameLink = Posts.findOne({url: postAttributes.url});
+        // Ensure user has logged in
+        if (!user)
+            throw new Meteor.Error(401, "You need to log in to post new stories");
+
+        // Ensure the post has a title
+        if(!postAttributes.title)
+            throw new Meteor.Error(422, "Please fill in a headline");
+
+        // Check that there are no posts with the same URL
+        if (postAttributes.url && postWithSameLink)
+            throw new Meteor.Error(302, "This link has already been posted", postWithSameLink._id);
+
+        // Pick out the whitelisted keys
+        var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+            userId: user._id,
+            author: user.username,
+            submitter: new Date().getTime()
+        });
+
+        var postId = Posts.insert(post);
+
+        return postId;
+
     }
-})
+
+});
