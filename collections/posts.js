b328dd75cb226d835143401d4d1b9130dc00ede0
bryan kennedy <bkennedy@smm.org>
2014-07-02 13:21:13 -0500

    Using MongoDB to check for post existence

--- collections/posts.js
+++ collections/posts.js
@@ -51,17 +51,10 @@ Meteor.methods({
         if (!user)
             throw new Meteor.Error(401, "You need to login to upvote");
 
-        var post = Posts.findOne(postId);
-        if (!post)
-            throw new Meteor.Error(422, 'Post not found');
-
-        /**
-         * Check to see if this user has already voted
-         */
-        if (_.include(post.upvoters, user._id))
-            throw new Meteor.Error(422, "You already upvoted this post");
-
-        Posts.update(post._id, {
+        Posts.update({
+            _id: postId,
+            upvoters: {$ne: user._id}
+        }, {
             $addToSet: {upvoters: user._id},
             $inc: {votes: 1}
         });
