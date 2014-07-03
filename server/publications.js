bbd4a560eb7ee7e423d0a9f1554c2c36888f4551
bryan kennedy <bkennedy@smm.org>
2014-07-03 10:24:04 -0500

    postId is published elsewhere now

--- server/publications.js
+++ server/publications.js
@@ -10,6 +10,6 @@ Meteor.publish('comments', function(postId) {
     return Comments.find({postId: postId});
 });
 
-Meteor.publish('notifications', function(postId) {
+Meteor.publish('notifications', function() {
     return Notifications.find({userId: this.userId});
 });
